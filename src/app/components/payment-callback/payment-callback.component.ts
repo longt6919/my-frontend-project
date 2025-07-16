e04f4de98ed808bc8870171c21a0f32cfc83f638
longt6919 <85840527+longt6919@users.noreply.github.com>
2025-07-16 15:24:47 +0700

    Đã thêm detail-product,footer, header, home, login, order, register, user-profile

---
+++ src/app/components/payment-callback/payment-callback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-callback',
+  templateUrl: './payment-callback.component.html',
+  styleUrls: ['./payment-callback.component.scss']
+})
+export class PaymentCallbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
